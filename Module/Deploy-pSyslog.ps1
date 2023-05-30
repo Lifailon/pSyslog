a3aa985b5964d3a866bd65f49b147b14faf487f7
Alex Kup <116945542+Lifailon@users.noreply.github.com>
2023-05-30 21:26:33 +0300

    Add files via upload

--- Module/Deploy-pSyslog.ps1
+++ Module/Deploy-pSyslog.ps1
@@ -5,8 +5,5 @@ if (!(Test-Path $path)) {
 New-Item $psm -ItemType "File" -Force | Out-Null
 New-Item $psd -ItemType "File" -Force | Out-Null
 }
-(Invoke-WebRequest "https://raw.githubusercontent.com/Lifailon/pSyslog/rsa/Module/pSyslog/0.2/pSyslog.psd1").Content | Out-File $psd -Encoding default -Force
-
-(Invoke-WebRequest "https://raw.githubusercontent.com/Lifailon/pSyslog/rsa/Module/pSyslog/0.2/pSyslog-0.2.psm1").Content | Out-File $psm -Encoding default -Force
-
-(New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/Lifailon/pSyslog/rsa/Module/pSyslog/0.2/pSyslog-0.2.psm1")
+(New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/Lifailon/pSyslog/rsa/Module/pSyslog/0.2/pSyslog.psd1") | Out-File $psd -Encoding default -Force
+(New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/Lifailon/pSyslog/rsa/Module/pSyslog/0.2/pSyslog-0.2.psm1") | Out-File $psm -Encoding default -Force
