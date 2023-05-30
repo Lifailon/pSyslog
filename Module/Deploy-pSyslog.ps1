69f3c1d4eb52ecd229dcceb228201d3a639d07a3
Alex Kup <116945542+Lifailon@users.noreply.github.com>
2023-05-30 21:07:02 +0300

    Add files via upload

--- Module/Deploy-pSyslog.ps1
+++ Module/Deploy-pSyslog.ps1
@@ -1,8 +1,9 @@
 $path = ($env:PSModulePath.Split(";")[0])+"\pSyslog\0.2\"
-$psm = "$path\pSyslog.psd1"
-$psd = "$path\pSyslog-0.2.psm1"
+$psd = "$path\pSyslog.psd1"
+$psm = "$path\pSyslog-0.2.psm1"
 if (!(Test-Path $path)) {
 New-Item $psm -ItemType "File" -Force | Out-Null
 New-Item $psd -ItemType "File" -Force | Out-Null
 }
-(iwr https://raw.githubusercontent.com/Lifailon/Veeam-REStat/rsa/Veeam-REStat/Veeam-REStat.psm1).Content | Out-File $path -Force
+(Invoke-WebRequest "https://raw.githubusercontent.com/Lifailon/pSyslog/rsa/Module/pSyslog/0.2/pSyslog.psd1").Content | Out-File $psd -Encoding default -Force
+(Invoke-WebRequest "https://raw.githubusercontent.com/Lifailon/pSyslog/rsa/Module/pSyslog/0.2/pSyslog-0.2.psm1").Content | Out-File $psm -Encoding default -Force
