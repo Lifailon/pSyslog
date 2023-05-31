3e3a9304a6ccd3ed03e24884245b7abd01e3f95d
Alex Kup <116945542+Lifailon@users.noreply.github.com>
2023-05-31 16:47:27 +0300

    Add files via upload

--- Module/Deploy-pSyslog.ps1
+++ Module/Deploy-pSyslog.ps1
@@ -1,9 +1,9 @@
-$path = ($env:PSModulePath.Split(";")[0])+"\pSyslog\0.2\"
+$path = ($env:PSModulePath.Split(";")[0])+"\pSyslog\0.3\"
 $psd = "$path\pSyslog.psd1"
-$psm = "$path\pSyslog-0.2.psm1"
+$psm = "$path\pSyslog-0.3.psm1"
 if (!(Test-Path $path)) {
 New-Item $psm -ItemType "File" -Force | Out-Null
 New-Item $psd -ItemType "File" -Force | Out-Null
 }
-(New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/Lifailon/pSyslog/rsa/Module/pSyslog/0.2/pSyslog.psd1") | Out-File $psd -Encoding default -Force
-(New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/Lifailon/pSyslog/rsa/Module/pSyslog/0.2/pSyslog-0.2.psm1") | Out-File $psm -Encoding default -Force
+(New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/Lifailon/pSyslog/rsa/Module/pSyslog/0.3/pSyslog.psd1") | Out-File $psd -Encoding default -Force
+(New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/Lifailon/pSyslog/rsa/Module/pSyslog/0.3/pSyslog-0.3.psm1") | Out-File $psm -Encoding default -Force
